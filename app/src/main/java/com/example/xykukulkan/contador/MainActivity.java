48a0ad214aba42fadae80c7b452bd37a88826a25
Eduardo Kukulkan <kukulkan1996@gmail.com>
2018-02-23 00:09:41 -0600

    ContadorApp: Se agregó el manejo de eventos a la interfaz de usuario gráfica.

--- app/src/main/java/com/example/xykukulkan/contador/MainActivity.java
+++ app/src/main/java/com/example/xykukulkan/contador/MainActivity.java
@@ -7,12 +7,27 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    protected Button btnContar;
+    private Button btnReiniciar;
+    private EditText editContador;
+    private int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        btnContar = (Button)this.findViewById(R.id.botonContar);
+        btnReiniciar = (Button)this.findViewById(R.id.botonReiniciar);
+        editContador = (EditText)this.findViewById(R.id.campoConteo);
+
+        btnContar.setOnClickListener( e -> {
+            contador = Integer.parseInt(editContador.getText().toString());
+            contador += 1;
+            editContador.setText(Integer.toString(contador));
+        });
+
+        btnReiniciar.setOnClickListener( e -> {
+            editContador.setText(Integer.toString(contador));
+        });
     }
 }
